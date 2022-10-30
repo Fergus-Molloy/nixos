3c51c8424b849915f3db26cbacfc950f7bedf31e
Fergus Molloy <fergus@molloy.xyz>
2022-10-30 23:50:00 +0000

    change shell to zsh

--- configuration.nix
+++ configuration.nix
@@ -98,10 +98,12 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  # enable zsh
+  programs.zsh.enable = true;
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.fergus = {
     isNormalUser = true;
-    description = "fergus";
+    shell = pkgs.zsh;
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       firefox
