c6a08e148c7b9f474eb4186ec0db2ccfe491e91d
Fergus Molloy <fergus@molloy.xyz>
2022-10-31 00:58:32 +0000

    add home manager maybe

--- flake.nix
+++ flake.nix
@@ -3,21 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
+    system = "x86_64-linux";
+  pkgs = import nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+  lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      fergus = lib.nixosSystem {
         inherit system;
-        config.allowUnfree = true;
-      };
-      lib = nixpkgs.lib;
-    in {
-      nixosConfigurations = {
-        fergus = lib.nixosSystem {
-          inherit system;
-          modules = [ ./configuration.nix ];
+        modules = [ 
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.fergus = {
+              imports = [ ./home.nix ];
+            };
+          }
+        ];
       };
     };
   };
